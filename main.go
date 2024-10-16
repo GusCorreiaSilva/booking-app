c255a9b698ed1345fe1d0c2bc3bb35810ca09047
Gustavo <gustaaavox@gmail.com>
2024-10-16 14:31:00 -0300

    Arrays, slices and loop

--- main.go
+++ main.go
@@ -6,33 +6,45 @@ import (
 )
 
 func main() {
-	var conferenceName = "Go Conference"
+	//2 pontos antes do '=' significa que é uma variavel comum, que não foi definido um valor previo.
+	//Ex - var conferenceName string = " " 
+  conferenceName := "Go Conference"
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("we have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get our tickets here to attend")
+	bookings := []string{}
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
-	//ask user for their name
-	fmt.Println("enter your first name")
-	fmt.Scan(&firstName)
 
-	fmt.Println("enter your last name")
-	fmt.Scan(&lastName)
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	fmt.Println("enter your best email address")
-	fmt.Scan(&email)
+
+		//ask user for their name
+		fmt.Println("enter your first name")
+		fmt.Scan(&firstName)
+
+		fmt.Println("enter your last name")
+		fmt.Scan(&lastName)
+
+		fmt.Println("enter your best email address")
+		fmt.Scan(&email)
+		
+		fmt.Println("Enter number of tickets")
+		fmt.Scan(&userTickets)
+		
+		remainingTickets -= userTickets
+		bookings = append(bookings, firstName + " " + lastName)
+
+		fmt.Printf("thank you  %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName,lastName, userTickets,email)
+		fmt.Printf("%v tickets are still available\n", remainingTickets)
+
+		fmt.Printf("This are all our bookings %v\n",bookings)
+	}
 	
-	fmt.Println("Enter number of tickets")
-	fmt.Scan(&userTickets)
-	
-	remainingTickets -= userTickets
-
-	fmt.Printf("thank you  %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName,lastName, userTickets,email)
-	fmt.Printf("%v tickets are still available\n", remainingTickets)
 }
